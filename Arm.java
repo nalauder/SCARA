d1e94f689ea91c7ea308230dbc19efb2c27ab9b6
Nick Lauder <nalauder@users.noreply.github.com>
2016-09-12 11:45:31 +1200

    progress on inverseKinematic
    starting to put value

--- Arm.java
+++ Arm.java
@@ -1,4 +1,3 @@
- 
 
 
 /**
@@ -14,7 +13,7 @@ import java.util.*;
 
 public class Arm
 {
-    
+
     // fixed arm parameters
     private int xm1;  // coordinates of the motor(measured in pixels of the picture)
     private int ym1;
@@ -30,19 +29,18 @@ public class Arm
     // motor 1, point 2
     private double pwm1_val_2; 
     private double theta1_val_2;
-    
+
     // motor 2, point 1
     private double pwm2_val_1; 
     private double theta2_val_1;
     // motor 2, point 2
     private double pwm2_val_2; 
     private double theta2_val_2;
-    
-    
+
     // current state of the arm
     private double theta1; // angle of the upper arm
     private double theta2;
-    
+
     private double xj1;     // positions of the joints
     private double yj1; 
     private double xj2;
@@ -50,7 +48,7 @@ public class Arm
     private double xt;     // position of the tool
     private double yt;
     private boolean valid_state; // is state of the arm physically possible?
-    
+
     /**
      * Constructor for objects of class Arm
      */
@@ -65,7 +63,7 @@ public class Arm
         theta2 = -90.0*Math.PI/180.0;
         valid_state = false;
     }
-  
+
     // draws arm on the canvas
     public void draw()
     {
@@ -77,7 +75,7 @@ public class Arm
         yj1 = ym1 + r*Math.sin(theta1);
         xj2 = xm2 + r*Math.cos(theta2);
         yj2 = ym2 + r*Math.sin(theta2);
-        
+
         //draw motors and write angles
         int mr = 20;
         UI.setLineWidth(5);
@@ -101,96 +99,96 @@ public class Arm
         // draw Field Of View
         UI.setColor(Color.GRAY);
         UI.drawRect(0,0,640,480);
-         
-       // it can b euncommented later when
-       // kinematic equations are derived
+
+        // it can b euncommented later when
+        // kinematic equations are derived
         if ( valid_state) {
-          // draw upper arms
-          UI.setColor(Color.GREEN);
-          UI.drawLine(xm1,ym1,xj1,yj1);
-          UI.drawLine(xm2,ym2,xj2,yj2);
-          //draw forearms
-          UI.drawLine(xj1,yj1,xt,yt);
-          UI.drawLine(xj2,yj2,xt,yt);
-          // draw tool
-          double rt = 20;
-          UI.drawOval(xt-rt/2,yt-rt/2,rt,rt);
+            // draw upper arms
+            UI.setColor(Color.GREEN);
+            UI.drawLine(xm1,ym1,xj1,yj1);
+            UI.drawLine(xm2,ym2,xj2,yj2);
+            //draw forearms
+            UI.drawLine(xj1,yj1,xt,yt);
+            UI.drawLine(xj2,yj2,xt,yt);
+            // draw tool
+            double rt = 20;
+            UI.drawOval(xt-rt/2,yt-rt/2,rt,rt);
         }
-        
-   }
-    
-   // calculate tool position from motor angles 
-   // updates variable in the class
-   public void directKinematic(){
-       
-       // midpoint between joints
-       //double  xa =.... ;
-       //double  ya =.... ;
-       // distance between joints
-       double d = 0;
-       if (d<2*r){
-           valid_state = true;
-         // half distance between tool positions
-         //double  h = ...;
-         //double alpha= ...;
-         // tool position
-        // double xt = ...;
-        // double yt = ...;
-         //  xt2 = xa - h.*cos(alpha-pi/2);
-         //  yt2 = ya - h.*sin(alpha-pi/2);
-       } else {
-           valid_state = false;
-        }
-       
+
     }
-    
+
+    // calculate tool position from motor angles 
+    // updates variable in the class
+    public void directKinematic(){
+
+        // midpoint between joints
+        //double  xa = (xj2-xj1)/2 + xj1;
+        //double  ya = (yj2-yj1)/2 + yj1;
+        // distance between joints
+        //double d = Math.sqrt((xj1-xj2)*(xj1-xj2)+(yj1-yj2)*(yj1-yj2));
+        double d = 0;
+        if (d<2*r){
+            valid_state = true;
+            // half distance between tool positions
+            //double  h = 0;
+            //double alpha= 0;
+            // tool position
+            double xt = 0;
+            double yt = 0;
+            //xt2 = xa - h*Math.cos(alpha-Math.PI/2); 
+            //yt2 = ya - h*Math.sin(alpha-Math.PI/2); 
+        } else {
+            valid_state = false;
+        }
+
+    }
+
     // motor angles from tool position
     // updetes variables of the class
     public void inverseKinematic(double xt_new,double yt_new){
-         
+
         valid_state = true;
         xt = xt_new;
         yt = yt_new;
         valid_state = true;
         double dx1 = xt - xm1; 
         double dy1 = yt - ym1;
-        // distance between pem and motor
-        double d1 = 0;
+        // distance between pen and motor
+        double d1 = Math.sqrt(dx1*dx1 + dy1+dy1);
         if (d1>2*r){
             //UI.println("Arm 1 - can not reach");
             valid_state = false;
             return;
         }
-        
+
         double l1 = d1/2;
         double h1 = Math.sqrt(r*r - d1*d1/4);
         // elbows positions
-        //xj1 = ...;
-        //yj1 = ...;
+        xj1 = ;
+        yj1 = ;
 
-        ///theta1 = ...;
+        theta1 = Math.cos(l1/r) + Math.tan(dy1/dx1);
         if ((theta1>0)||(theta1<-Math.PI)){
             valid_state = false;
             //UI.println("Ange 1 -invalid");
             return;
         }
-        
+
         // theta12 = atan2(yj12 - ym1,xj12-xm1);
         double dx2 = xt - xm2; 
         double dy2 = yt - ym2;
-        double d2 = 0;
+        double d2 = Math.sqrt(dx2*dx2 + dy2+dy2);
         if (d2>2*r){
-           // UI.println("Arm 2 - can not reach");
+            // UI.println("Arm 2 - can not reach");
             valid_state = false;
             return;
         }
 
         double l2 = d2/2;
-        
         double h2 = Math.sqrt(r*r - d2*d2/4);
         // elbows positions
-        xj2 = 0;
-        yj2 = 0;
+        //xj2 = 0;
+        //yj2 = 0;
         // motor angles for both 1st elbow positions
         theta2 = 0;
         if ((theta2>0)||(theta2<-Math.PI)){
@@ -198,12 +196,12 @@ public class Arm
             //UI.println("Ange 2 -invalid");
             return;
         }
-        
+
         //UI.printf("xt:%3.1f, yt:%3.1f\n",xt,yt);
         //UI.printf("theta1:%3.1f, theta2:%3.1f\n",theta1*180/Math.PI,theta2*180/Math.PI);
         return;
     }
-    
+
     // returns angle of motor 1
     public double get_theta1(){
         return theta1;
@@ -217,7 +215,7 @@ public class Arm
         theta1 = t1;
         theta2 = t2;
     }
-    
+
     // returns motor control signal
     // for motor to be in position(angle) theta1
     // linear intepolation
@@ -231,5 +229,5 @@ public class Arm
         //pwm = (int)(pwm2_90 + (theta2 - 90)*pwm2_slope);
         return pwm;
     }
-    
- }
+
+}
